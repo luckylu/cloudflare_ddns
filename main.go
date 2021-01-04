4b463997f3397b61650b4fe7a2461bfe271c71b7
Michael <kobelu1990@gmail.com>
2021-01-04 18:06:46 +0800

    get ip

--- main.go
+++ main.go
@@ -1,63 +1,87 @@
 package main
 
 import (
+  "encoding/json"
+  "flag"
   "fmt"
   "gopkg.in/h2non/gentleman.v2"
-  "gopkg.in/h2non/gentleman.v2/plugins/headers"
   "gopkg.in/h2non/gentleman.v2/plugins/body"
-  "encoding/json"
+  "gopkg.in/h2non/gentleman.v2/plugins/headers"
   "os"
-  "flag"
 )
-type Config struct{
-  AuthToken string
-  DnsType string
+
+type Config struct {
+  AuthToken      string
+  DnsType        string
   FullDomainName string
-  Ttl int
-  Priority int
-  Proxied bool
-  ZoneId string
+  Ttl            int
+  Priority       int
+  Proxied        bool
+  ZoneId         string
 }
 
-type ResponseResult struct{
-  Success bool `json:"success"`
-  Errors interface{} `json:"errors"`
-  Messages interface{} `json:"messages"`
-  Result ResultDetail `json:"result"`
+type ResponseResult struct {
+  Success  bool         `json:"success"`
+  Errors   interface{}  `json:"errors"`
+  Messages interface{}  `json:"messages"`
+  Result   ResultDetail `json:"result"`
 }
 
-type ResponseResults struct{
-  Success bool `json:"success"`
-  Errors interface{} `json:"errors"`
-  Messages interface{} `json:"messages"`
-  Result []ResultDetail `json:"result"`
+type ResponseResults struct {
+  Success  bool           `json:"success"`
+  Errors   interface{}    `json:"errors"`
+  Messages interface{}    `json:"messages"`
+  Result   []ResultDetail `json:"result"`
 }
 
-type ResultDetail struct{
-  Id string `json:"id"`
-  Type string `json:"type"`
-  Name string `json:"name"`
-  Content string `json:"content"`
-  Proxiable bool `json:"proxiable"`
-  Ttl int `json:"ttl"`
-  Locked bool `json:"locked"`
-  ZoneId string `json:"zone_id"`
-  ZoneName string `json:"zone_name"`
-  CreatedOn string `json:"created_on"`
-  ModifiedOn string `json:"modified_on"`
-  Data interface{} `json:"data"`
-  Meta interface{} `json:"meta"`
+type ResultDetail struct {
+  Id         string      `json:"id"`
+  Type       string      `json:"type"`
+  Name       string      `json:"name"`
+  Content    string      `json:"content"`
+  Proxiable  bool        `json:"proxiable"`
+  Ttl        int         `json:"ttl"`
+  Locked     bool        `json:"locked"`
+  ZoneId     string      `json:"zone_id"`
+  ZoneName   string      `json:"zone_name"`
+  CreatedOn  string      `json:"created_on"`
+  ModifiedOn string      `json:"modified_on"`
+  Data       interface{} `json:"data"`
+  Meta       interface{} `json:"meta"`
 }
 
 func main() {
   c := flag.String("c", "./config/config.json", "Specify the configuration file.")
   flag.Parse()
   config := LoadConfiguration(*c)
-  _, result := GetRecords(config)
-  fmt.Println(result)
+  ip := GetIp()
+  ok, records := GetRecords(config)
+  recordCreated := false
+  var recordId string
+  for resultA := range records.Result {
+    if resultA.Name == config.FullDomainName {
+      recordCreated = true
+      recordId = resultA.Id
+    }
+  }
+  if recordCreated {
+    UpdateRecord(config, recordId, ip)
+  } else {
+    CreateRecord(config, ip)
+  }
 }
 
-func GetRecords(config Config) (ok bool, result ResponseResults){
+func GetIp() (ip string) {
+  cli := gentleman.New()
+  res, err := cli.Request().Method("GET").URL("http://members.3322.org/dyndns/getip").Send()
+  if err != nil {
+    panic(err.Error())
+  }
+  ip = res.String()
+  return
+}
+
+func GetRecords(config Config) (ok bool, result ResponseResults) {
   cli := gentleman.New()
   authorization := "Bearer " + config.AuthToken
   customHeaders := map[string]string{"Authorization": authorization, "Content-Type": "application/json"}
@@ -68,8 +92,8 @@ func GetRecords(config Config) (ok bool, result ResponseResults){
     return
   }
   if !res.Ok {
-  json.Unmarshal([]byte(res.String()), &result)
-  ok = false
+    json.Unmarshal([]byte(res.String()), &result)
+    ok = false
     return
   }
 
@@ -78,7 +102,7 @@ func GetRecords(config Config) (ok bool, result ResponseResults){
   return
 }
 
-func CreateRecord(config Config, ip string) (ok bool, result ResponseResult){
+func CreateRecord(config Config, ip string) (ok bool, result ResponseResult) {
   cli := gentleman.New()
 
   // Define a custom header
@@ -86,18 +110,18 @@ func CreateRecord(config Config, ip string) (ok bool, result ResponseResult){
   customHeaders := map[string]string{"Authorization": authorization, "Content-Type": "application/json"}
   cli.Use(headers.SetMap(customHeaders))
 
-  data := map[string]interface{}{"type": config.DnsType, "name": config.FullDomainName, "content": ip,"ttl": config.Ttl,"priority":config.Priority,"proxied":config.Proxied}
+  data := map[string]interface{}{"type": config.DnsType, "name": config.FullDomainName, "content": ip, "ttl": config.Ttl, "priority": config.Priority, "proxied": config.Proxied}
   cli.Use(body.JSON(data))
 
-    // Perform the request
+  // Perform the request
   res, err := cli.Request().Method("POST").URL("https://api.cloudflare.com/client/v4/zones/" + config.ZoneId + "/dns_records").Send()
   if err != nil {
     ok = false
     return
   }
   if !res.Ok {
-  json.Unmarshal([]byte(res.String()), &result)
-  ok = false
+    json.Unmarshal([]byte(res.String()), &result)
+    ok = false
     return
   }
 
@@ -106,8 +130,7 @@ func CreateRecord(config Config, ip string) (ok bool, result ResponseResult){
   return
 }
 
-func UpdateRecord(config Config, recordId string, ip string) (ok bool, result ResponseResult){
-  fmt.Println(config)
+func UpdateRecord(config Config, recordId string, ip string) (ok bool, result ResponseResult) {
   cli := gentleman.New()
 
   // Define a custom header
@@ -115,18 +138,18 @@ func UpdateRecord(config Config, recordId string, ip string) (ok bool, result Re
   customHeaders := map[string]string{"Authorization": authorization, "Content-Type": "application/json"}
   cli.Use(headers.SetMap(customHeaders))
 
-  data := map[string]interface{}{"type": config.DnsType, "name": config.FullDomainName, "content": ip,"ttl": config.Ttl,"priority":config.Priority,"proxied":config.Proxied}
+  data := map[string]interface{}{"type": config.DnsType, "name": config.FullDomainName, "content": ip, "ttl": config.Ttl, "priority": config.Priority, "proxied": config.Proxied}
   cli.Use(body.JSON(data))
 
-    // Perform the request
+  // Perform the request
   res, err := cli.Request().Method("PUT").URL("https://api.cloudflare.com/client/v4/zones/" + config.ZoneId + "/dns_records/" + recordId).Send()
   if err != nil {
     ok = false
     return
   }
   if !res.Ok {
-  json.Unmarshal([]byte(res.String()), &result)
-  ok = false
+    json.Unmarshal([]byte(res.String()), &result)
+    ok = false
     return
   }
 
@@ -139,7 +162,7 @@ func LoadConfiguration(file string) (conf Config) {
   configFile, err := os.Open(file)
   defer configFile.Close()
   if err != nil {
-      fmt.Println(err.Error())
+    fmt.Println(err.Error())
   }
   jsonParser := json.NewDecoder(configFile)
   jsonParser.Decode(&conf)
